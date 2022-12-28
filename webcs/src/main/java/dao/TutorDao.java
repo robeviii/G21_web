456277ca86de75cb63226914b03b0ff7837bce9d
VicMoraGit <victormoradj@hotmmail.com>
2022-12-28 23:07:36 +0100

    feat: Patron DAO para tutor:
    - Implementados metodos get y getall

--- webcs/src/main/java/dao/TutorDao.java
+++ webcs/src/main/java/dao/TutorDao.java
@@ -5,6 +5,11 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 import model.Tutor;
 import util.DbUtil;
@@ -27,12 +32,61 @@ public class TutorDao implements Dao<Tutor>{
     
     @Override
     public Tutor get(String email) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        Tutor tutor = new Tutor();
+        if (connection != null)
+        {
+            try {
+                PreparedStatement preparedStatement = connection.prepareStatement("select * from tutor where email=?;");
+                preparedStatement.setString(1, email);
+                ResultSet rs = preparedStatement.executeQuery();
+                if (rs.next()) {
+                    tutor.setEmail(email);
+                    tutor.setNombre(rs.getString("nombre"));
+                    tutor.setApellido(rs.getString("apellido"));
+                    tutor.setPassword(rs.getString("password"));
+                }
+
+            } catch (SQLException e) {
+                Log.logdb.error("SQL Exception: " + e + "\n");
+            }
+            return tutor;
+        }
+        else
+        {
+            Log.logdb.error("No hay conexion con la bbdd\n");
+            return null;
+        }
     }
 
     @Override
     public List<Tutor> getAll() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        List<Tutor> tutores = new ArrayList<Tutor>();
+        if (connection != null)
+        {
+            try {
+                Statement statement = connection.createStatement();
+                ResultSet rs = statement.executeQuery("select * from tutor;");
+                while (rs.next()) {
+                    Tutor tutor = new Tutor();
+                    tutor.setEmail(rs.getString("email"));
+                    tutor.setNombre(rs.getString("nombre"));
+                    tutor.setApellido(rs.getString("apellido"));
+                    tutor.setPassword(rs.getString("password"));
+                
+        
+                    
+                    tutores.add(tutor);
+                }
+            } catch (SQLException e) {
+                Log.logdb.error("SQL Exception: " + e + "\n");            
+            }
+            return tutores;
+        }
+        else
+        {
+            Log.logdb.error("No hay conexion con la bbdd\n");
+            return null;
+        }
     }
 
     @Override
