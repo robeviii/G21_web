bee5bddb9fecd34445b409714efbeb05bc318d1c
VicMoraGit <victormoradj@hotmmail.com>
2022-12-29 03:05:02 +0100

    feat: implementacion sistema de login basico - Conexion a BBDD de prueba "ingenieros_al_peso" correcta - Añadidas dependencias del driver jdbc - metodo del login implementado - Si el usuario existe aparece un panel de control de prueba

--- webcs/src/main/java/controller/LoginServlet.java
+++ webcs/src/main/java/controller/LoginServlet.java
@@ -5,6 +5,7 @@ package controller;
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
 
+import dao.TutorDao;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.annotation.WebServlet;
 import java.io.IOException;
@@ -12,6 +13,8 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import model.Tutor;
+import util.Log;
 
 /**
  *
@@ -22,7 +25,13 @@ import jakarta.servlet.http.HttpServletResponse;
   urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
 
- 
+    private TutorDao dao;
+    
+    public LoginServlet() {
+        super();
+        dao = new TutorDao();
+    }
+    
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
@@ -32,6 +41,7 @@ public class LoginServlet extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
+    
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -51,6 +61,25 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        
+        String email = request.getParameter("email");
+        String pwd = request.getParameter("password");
+        Tutor tutor = dao.get(email);
+        if (tutor != null && tutor.getPassword().equals(pwd)){
+            Log.log.info("tutor logueado con exito");
+            request.setAttribute("nombre", tutor.getNombre());
+            request.setAttribute("apellido", tutor.getApellido());
+            request.setAttribute("email", email);
+             
+            RequestDispatcher view = request.getRequestDispatcher("test/testDashboard.jsp");            
+            view.forward(request, response);
+                
+
+        }
+        else {
+            System.out.println(tutor.getPassword().equals(pwd));
+            System.out.println(pwd);
+        }
     }
 
     /**
