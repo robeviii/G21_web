32ee20f9d01e1d2f28589963fbc91030f94fd389
VicMoraGit <victormoradj@hotmmail.com>
2022-12-29 18:22:00 +0100

    feat: Modelo alumno

---
+++ webcs/src/main/java/model/Alumno.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author victor
+ */
+public class Alumno extends Usuario{
+    private int id;
+    private String dni;
+    private double nota_media;
+    private String nombre;
+    private int id_tutor;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public double getNota_media() {
+        return nota_media;
+    }
+
+    public void setNota_media(double nota_media) {
+        this.nota_media = nota_media;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getId_tutor() {
+        return id_tutor;
+    }
+
+    public void setId_tutor(int id_tutor) {
+        this.id_tutor = id_tutor;
+    }
+    
+    
+}
